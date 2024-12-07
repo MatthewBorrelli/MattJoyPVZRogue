115b460a9a0261848aa0245f9dc5c8d087562e7e
Joyfully_Tired <djthunderbolts@gmail.com>
2024-12-06 21:28:28 -0800

    Made it so you can place the different plants and made prefabs of them (no functionality)

--- PVZRogue/Assets/Scrjpts/Ui/Current Defenses.cs
+++ PVZRogue/Assets/Scrjpts/Ui/Current Defenses.cs
@@ -4,13 +4,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CurrentDefenses : MonoBehaviour
 {
     public GameObject[] defences = new GameObject[6];
+    public GameObject[] buttons = new GameObject[6];
     public ResourceManager reMan;
 
-
+    void Start(){
+        for(int i = 0; i < 6; i++){
+            buttons[i].GetComponent<Image>().sprite = defences[i].GetComponent<DefenseUnit>().iconSprite;
+            buttons[i].transform.GetChild(0).GetComponent<TMP_Text>().text = "Cost " + defences[i].GetComponent<DefenseUnit>().cost;
+        }
+    }
 
     public void BuyUnit(int index){
         DefenseUnit unit = defences[index].GetComponent<DefenseUnit>();
