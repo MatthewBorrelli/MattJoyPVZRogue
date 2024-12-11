0f5cd19456e4de9e6f51a3e2041d06690d85fb40
Joyfully_Tired <djthunderbolts@gmail.com>
2024-12-11 11:52:46 -0800

    Archer done

---
+++ PVZRogue/Assets/Scrjpts/Defensive Units/Archer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Name: Matthew Borrelli
+/// Last Updated: 12/11/24
+/// Function: Makes the archer shoot his arrows
+/// </summary>
+
+public class Archer : MonoBehaviour
+{
+    public GameObject arrow;
+    
+    RaycastHit hit;
+    public float rayLength = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Shoot", 4f, 4f);
+    }
+
+    public void Shoot()
+    {
+        if(Physics.Raycast(transform.position, Vector3.right, out hit, rayLength)){
+            Instantiate(arrow, transform.position + (Vector3.back * 0.1f), transform.rotation);
+        }
+    }  
+}
