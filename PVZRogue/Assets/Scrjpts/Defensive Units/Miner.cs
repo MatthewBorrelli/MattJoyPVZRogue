455c86e0142a0bd96004cc10b21f301d74db18c3
Joyfully_Tired <djthunderbolts@gmail.com>
2024-12-11 11:26:09 -0800

    MINERSSSS???!!!!

---
+++ PVZRogue/Assets/Scrjpts/Defensive Units/Miner.cs
@@ -0,0 +1,29 @@
+//Joy Schulze
+//December 11th
+//Lets the miner unit periodically spawn gold. Gold is randomly double value.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Miner : MonoBehaviour
+{
+    public GameObject gold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnGold", 5f, 5f);
+    }
+
+    private void SpawnGold(){
+        int rand = Random.Range(1, 10);
+
+        if(rand != 10)
+            gold.GetComponent<Gold>().SetValueAndFallTime(50, 0.2f);
+        else
+            gold.GetComponent<Gold>().SetValueAndFallTime(100, 0.2f);
+        
+        Instantiate(gold, transform.position + (Vector3.back * 2), transform.rotation);
+        gold.GetComponent<Gold>().SetValueAndFallTime(50, 0.2f);
+    }
+}
